3aef96b7347d66b92eccc5b12d489bcc484e2888
William Waites <wwaites@tardis.ed.ac.uk>
2014-10-15 23:12:49 +0100

    - fix import for basic Process  - override is needed for the step function  - Math.floor(t) is more succinct as just t.floor

--- src/main/scala/uk/ac/ed/inf/mois/clocks/MammalianCircadianClock.scala
+++ src/main/scala/uk/ac/ed/inf/mois/clocks/MammalianCircadianClock.scala
@@ -1,24 +1,24 @@
- /* 
+ /*
   *  MOIS Examples: Mammalian Circadian Clock (Forger-Peskin 2002)
   *  Copyright (C) 2014 University of Edinburgh School of Informatics
-  * 
+  *
   *  This program is free software: you can redistribute it and/or modify
   *  it under the terms of the GNU General Public License as published by
   *  the Free Software Foundation, either version 3 of the License, or
   *  (at your option) any later version.
-  * 
+  *
   *  This program is distributed in the hope that it will be useful,
   *  but WITHOUT ANY WARRANTY; without even the implied warranty of
   *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   *  GNU General Public License for more details.
-  * 
+  *
   *  You should have received a copy of the GNU General Public License
   *  along with this program. If not, see <http://www.gnu.org/licenses/>.
   */
 
 package uk.ac.ed.inf.mois.clocks
 
-import uk.ac.ed.inf.mois.{Model, ODE, ProcessGroup, VarCalc, Math}
+import uk.ac.ed.inf.mois.{Model, ODE, Process, ProcessGroup, VarCalc, Math}
 import uk.ac.ed.inf.mois.sched.NaiveScheduler
 import spire.implicits._
 import uk.ac.ed.inf.mois.implicits._
@@ -609,8 +609,8 @@ class Light extends Process {
   *
   */
 
-  def step(t: Double, tau: Double) {
-    if (Math.floor(t)%2 == 0) {
+  override def step(t: Double, tau: Double) {
+    if (t.floor % 2 == 0) {
       L := 0.000339
     } else {
       L := 0
